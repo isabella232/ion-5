387c9e174dd7b2ec19c14b20dd7a72bffb65eaab
cretin45 <cretin45@gmail.com>
2014-09-26 16:32:56 -0700

    Ion: fix build for GalleryNext
    Change-Id: I798db5f249a6e17b26bb548a382705ca42cf117f

--- ion/Android.mk
+++ ion/Android.mk
@@ -18,10 +18,16 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_STATIC_JAVA_LIBRARIES := AndroidAsync gson android-support-v4
+LOCAL_STATIC_JAVA_LIBRARIES := AndroidAsync android-support-v4 local_gson
 
 LOCAL_MODULE := ion
 LOCAL_SDK_VERSION := 19
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
 include $(BUILD_STATIC_JAVA_LIBRARY)
+include $(CLEAR_VARS)
+
+LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES := \
+    local_gson:libs/gson-2.3.jar
+
+include $(BUILD_MULTI_PREBUILT)
